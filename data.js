01860118e83f1ea837b01d29c344d25334f78e0d
LofA <contact.lofa@gmail.com>
2023-08-20 23:39:11 +0700

    resolve conflict imgaes, coding dropdown menu and reconfigure SectionProduct

--- data.js
+++ data.js
@@ -1,123 +1,483 @@
+import img1 from "./src/assets/imgs/img1.jpg";
+import img1_1 from "./src/assets/imgs/img1_1.jpg";
+import img2 from "./src/assets/imgs/img2.jpg";
+import img2_1 from "./src/assets/imgs/img2_1.jpg";
+import img3 from "./src/assets/imgs/img3.png";
+import img3_1 from "./src/assets/imgs/img3_1.jpg";
+import img4 from "./src/assets/imgs/img4.jpg";
+import img4_1 from "./src/assets/imgs/img4_1.jpg";
+import img5 from "./src/assets/imgs/img5.jpg";
+import img5_1 from "./src/assets/imgs/img5_1.jpg";
+import img6 from "./src/assets/imgs/img6.png";
+import img6_1 from "./src/assets/imgs/img6_1.jpg";
+import img7 from "./src/assets/imgs/img7.jpg";
+import img7_1 from "./src/assets/imgs/img7_1.jpg";
+import img8 from "./src/assets/imgs/img8.jpg";
+import img8_1 from "./src/assets/imgs/img8_1.jpg";
+import img9 from "./src/assets/imgs/img9.png";
+import img9_1 from "./src/assets/imgs/img9_1.png";
+import img10 from "./src/assets/imgs/img10.png";
+import img10_1 from "./src/assets/imgs/img10_1.png";
+import img11 from "./src/assets/imgs/img11.png";
+import img11_1 from "./src/assets/imgs/img11_1.jpg";
+import img12 from "./src/assets/imgs/img12.png";
+import img12_1 from "./src/assets/imgs/img12_1.png";
+import img13 from "./src/assets/imgs/img13.jpg";
+import img13_1 from "./src/assets/imgs/img13_1.jpg";
+import img14 from "./src/assets/imgs/img14.jpg";
+import img14_1 from "./src/assets/imgs/img14_1.jpg";
+import img15 from "./src/assets/imgs/img15.jpg";
+import img15_1 from "./src/assets/imgs/img15_1.jpg";
+import img16 from "./src/assets/imgs/img16.jpg";
+import img16_1 from "./src/assets/imgs/img16_1.jpg";
+import img17 from "./src/assets/imgs/img17.png";
+import img17_1 from "./src/assets/imgs/img17_1.png";
+import img18 from "./src/assets/imgs/img18.jpg";
+import img18_1 from "./src/assets/imgs/img18_1.jpg";
+import img19 from "./src/assets/imgs/img19.jpg";
+import img19_1 from "./src/assets/imgs/img19_1.png";
+import img20 from "./src/assets/imgs/img20.jpg";
+import img20_1 from "./src/assets/imgs/img20_1.jpg";
+import img21 from "./src/assets/imgs/img21.jpg";
+import img21_1 from "./src/assets/imgs/img21_1.jpg";
+import img22 from "./src/assets/imgs/img22.jpg";
+import img22_1 from "./src/assets/imgs/img22_1.jpg";
+import img23 from "./src/assets/imgs/img23.jpg";
+import img23_1 from "./src/assets/imgs/img23_1.jpg";
+import img24 from "./src/assets/imgs/img24.jpg";
+import img24_1 from "./src/assets/imgs/img24_1.jpg";
+import img25 from "./src/assets/imgs/img25.jpg";
+import img25_1 from "./src/assets/imgs/img25_1.jpg";
+import img26 from "./src/assets/imgs/img26.jpg";
+import img26_1 from "./src/assets/imgs/img26_1.jpg";
+import img27 from "./src/assets/imgs/img27.jpg";
+import img27_1 from "./src/assets/imgs/img27_1.jpg";
+import img28 from "./src/assets/imgs/img28.jpg";
+import img28_1 from "./src/assets/imgs/img28_1.jpg";
+import img29 from "./src/assets/imgs/img29.jpg";
+import img29_1 from "./src/assets/imgs/img29_1.jpg";
+import img30 from "./src/assets/imgs/img30.png";
+import img30_1 from "./src/assets/imgs/img30_1.png";
+import img31 from "./src/assets/imgs/img31.png";
+import img31_1 from "./src/assets/imgs/img31_1.jpg";
+import img32 from "./src/assets/imgs/img32.jpg";
+import img32_1 from "./src/assets/imgs/img32_1.jpg";
+import img33 from "./src/assets/imgs/img33.png";
+import img33_1 from "./src/assets/imgs/img33_1.png";
+import img34 from "./src/assets/imgs/img34.png";
+import img34_1 from "./src/assets/imgs/img34_1.png";
+import img35 from "./src/assets/imgs/img35.png";
+import img35_1 from "./src/assets/imgs/img35_1.png";
+import img36 from "./src/assets/imgs/img36.png";
+import img36_1 from "./src/assets/imgs/img36_1.png";
+import img37 from "./src/assets/imgs/img37.png";
+import img37_1 from "./src/assets/imgs/img37_1.png";
+import img38 from "./src/assets/imgs/img38.png";
+import img38_1 from "./src/assets/imgs/img38_1.png";
+import img39 from "./src/assets/imgs/img39.jpg";
+import img39_1 from "./src/assets/imgs/img39_1.jpg";
+import img40 from "./src/assets/imgs/img40.png";
+import img40_1 from "./src/assets/imgs/img40_1.png";
 const products = [
   {
     id: 1,
-    image1: "./src/assets/imgs/img1.jpg",
-    image2: "./src/assets/imgs/img1_1.jpg",
-    title: "Bộ keycap cherry doraemon",
-    price: 525000,
-    salePercent: 24,
-    brand: "Stooges",
-    type: "Keycap bộ",
+    image1: img1,
+    image2: img1_1,
+    title: "Bộ keycap xda commando",
+    price: 499000,
+    salePercent: 14,
+    brand: "CMK",
+    type: "keycap bộ",
   },
   {
     id: 2,
-    image1: "./src/assets/imgs/img2.jpg",
-    image2: "./src/assets/imgs/img2_1.jpg",
-    title: "Túi đựng bàn phím, chống sốc, chống xước",
-    price: 80000,
-    salePercent: 14,
-    brand: "Kelowna",
-    type: "Phụ kiện bàn phím",
+    image1: img2,
+    image2: img2_1,
+    title: "BỘ KEYCAP SA DANDELION",
+    price: 1200000,
+    salePercent: null,
+    brand: "OSIRIS",
+    type: "keycap bộ",
   },
   {
     id: 3,
-    image1: "./src/assets/imgs/img3.png",
-    image2: "./src/assets/imgs/img3_1.jpg",
-    title: "Bộ keycap silent forest",
-    price: 780000,
+    image1: img3,
+    image2: img3_1,
+    title: "SWITCH WUQUE STUDIO",
+    price: 9400,
     salePercent: null,
-    brand: "Cmk",
-    type: "Keycap bộ",
+    brand: "WUQUE STUDIO",
+    type: "switch",
   },
   {
     id: 4,
-    image1: "./src/assets/imgs/img4.jpg",
-    image2: "./src/assets/imgs/img4_1.jpg",
-    title: "Bàn phím cơ cmk75 | fl-esports",
-    price: 2550000,
-    salePercent: 14,
-    brand: "Fl - esports",
-    type: "Bàn phím cơ",
+    image1: img4,
+    image2: img4_1,
+    title: "BỘ KEYCAP PS DESIGN",
+    price: 800000,
+    salePercent: 6,
+    brand: "TUT",
+    type: "keycap bộ",
   },
   {
     id: 5,
-    image1: "./src/assets/imgs/img5.jpg",
-    image2: "./src/assets/imgs/img5_1.jpg",
-    title: "Bộ keycap chocolate donut",
-    price: 699000,
-    salePercent: null,
-    brand: "Cmk",
-    type: "Keycap bộ",
+    image1: img5,
+    image2: img5_1,
+    title: "SWITCH JELLY STRAWBERRY V2",
+    price: 7500,
+    salePercent: 20,
+    brand: null,
+    type: "switch",
   },
   {
     id: 6,
-    image1: "./src/assets/imgs/img6.png",
-    image2: "./src/assets/imgs/img6_1.jpg",
-    title: "Switch ws morandi",
-    price: 10500,
-    salePercent: 14,
-    brand: "Wuque studio",
-    type: "Switch",
+    image1: img6,
+    image2: img6_1,
+    title: "BÀN PHÍM CƠ TÙY CHỈNH KIT GMK67 - ZOUYA",
+    price: 1050000,
+    salePercent: 19,
+    brand: "ZOUYA",
+    type: "kit",
   },
   {
     id: 7,
-    image1: "./src/assets/imgs/img7.jpg",
-    image2: "./src/assets/imgs/img7_1.jpg",
-    title: "Bàn phím cơ custom infiverse infi75",
-    price: 2500000,
-    salePercent: null,
-    brand: "Infiverse",
-    type: "Bàn phím cơ",
+    image1: img7,
+    image2: img7_1,
+    title: "BỘ KEYCAP CMK BOW/WOB/RED",
+    price: 400000,
+    salePercent: 12,
+    brand: "AIFEI",
+    type: "keycap bộ",
   },
   {
     id: 8,
-    image1: "./src/assets/imgs/img8.jpg",
-    image2: "./src/assets/imgs/img8_1.jpg",
-    title: "Plate nhôm cho bàn phím cơ",
-    price: 270000,
-    salePercent: null,
-    brand: "Kicap",
-    type: "Phụ kiện",
+    image1: img8,
+    image2: img8_1,
+    title: "BỘ KEYCAP NAHIDA GRASS GOD",
+    price: 890000,
+    salePercent: 14,
+    brand: "XT-DESIGN",
+    type: "keycap bộ",
   },
   {
     id: 9,
-    image1: "./src/assets/imgs/img9.jpg",
-    image2: "./src/assets/imgs/img9_1.jpg",
-    title: "Bộ keycap sa colourful dream",
-    price: 1100000,
+    image1: img9,
+    image2: img9_1,
+    title: "BÀN PHÍM CƠ TÙY CHỈNH KIT V65 R2",
+    price: 2400000,
     salePercent: null,
-    brand: "Osiris",
-    type: "Keycap bộ",
+    brand: "CIDOO",
+    type: "kit",
   },
   {
     id: 10,
-    image1: "./src/assets/imgs/img10.png",
-    image2: "./src/assets/imgs/img10_1.jpg",
-    title: "Bàn phím cơ kit finalkey v81 plus",
-    price: 1100000,
-    salePercent: 9,
-    brand: "Finalkey",
-    type: "Kit",
+    image1: img10,
+    image2: img10_1,
+    title: "BỘ KEYCAP CMK MODERN DOLCH LIGHT - RETRO",
+    price: 400000,
+    salePercent: null,
+    brand: "AIFEI",
+    type: "keycap bộ",
   },
   {
     id: 11,
-    image1: "./src/assets/imgs/img11.png",
-    image2: "./src/assets/imgs/img11_1.png",
-    title: "Bộ keycap p5 persona5",
-    price: 900000,
-    salePercent: 11,
-    brand: "Xt-design",
-    type: "Keycap bộ",
+    image1: img11,
+    image2: img11_1,
+    title: "SWITCH AKKO CREAM YELLOW/BLUE V3",
+    price: 7000,
+    salePercent: 26,
+    brand: "AKKO",
+    type: "switch",
   },
   {
     id: 12,
-    image1: "./src/assets/imgs/img12.jpg",
-    image2: "./src/assets/imgs/img12_1.jpg",
-    title: "Bộ keycap p5 persona5",
+    image1: img12,
+    image2: img12_1,
+    title: "BÀN PHÍM CƠ GP75 FL-ESPORTS",
+    price: 2350000,
+    salePercent: null,
+    brand: "FL - ESPORTS",
+    type: "bàn phím cơ",
+  },
+  {
+    id: 13,
+    image1: img13,
+    image2: img13_1,
+    title: "BỘ KEYCAP CMK MODERN DOLCH",
+    price: 450000,
+    salePercent: 16,
+    brand: "AIFEI",
+    type: "keycap bộ",
+  },
+  {
+    id: 14,
+    image1: img14,
+    image2: img14_1,
+    title: "BỘ KEYCAP XDA EMILIA",
+    price: 500000,
+    salePercent: 20,
+    brand: "SOY SAUCE",
+    type: "keycap bộ",
+  },
+  {
+    id: 15,
+    image1: img15,
+    image2: img15_1,
+    title: "BỘ KEYCAP CHERRY RICE SHOWER",
+    price: 830000,
+    salePercent: 10,
+    brand: "XT-DESIGN",
+    type: "keycap bộ",
+  },
+  {
+    id: 16,
+    image1: img16,
+    image2: img16_1,
+    title: "SWITCH KTT MATCHA - TACTILE",
+    price: 10000,
+    salePercent: 10,
+    brand: "KTT",
+    type: "switch",
+  },
+  {
+    id: 17,
+    image1: img17,
+    image2: img17_1,
+    title: "BÀN PHÍM CƠ CUSTOM MONSGEEK M3 NHÔM CNC",
+    price: 1800000,
+    salePercent: null,
+    brand: "MONSGEEK",
+    type: "bàn phím cơ",
+  },
+  {
+    id: 18,
+    image1: img18,
+    image2: img18_1,
+    title: "BỘ KEYCAP BOCCHI THE ROCK",
+    price: 850000,
+    salePercent: 8,
+    brand: "XT-DESIGN",
+    type: "keycap bộ",
+  },
+  {
+    id: 19,
+    image1: img19,
+    image2: img19_1,
+    title: "BÀN PHÍM CƠ KIT LMK81 - ZUOYA",
+    price: 2300000,
+    salePercent: null,
+    brand: "ZUOYA",
+    type: "bàn phím cơ",
+  },
+  {
+    id: 20,
+    image1: img20,
+    image2: img20_1,
+    title: "SWITCH GATERON PURE BERRY X MODERN GREY LINEAR SWITCH",
     price: 525000,
+    salePercent: 14,
+    brand: "GATERON",
+    type: "switch",
+  },
+  {
+    id: 21,
+    image1: img21,
+    image2: img21_1,
+    title: "BỘ KEYCAP NEWTOY X WUKDS 1970",
+    price: 1200000,
+    salePercent: 12,
+    brand: "WUKDS",
+    type: "keycap bộ",
+  },
+  {
+    id: 22,
+    image1: img22,
+    image2: img22_1,
+    title: "BỘ KEYCAP RETRO 1980",
+    price: 800000,
+    salePercent: 6,
+    brand: "M7",
+    type: "keycap bộ",
+  },
+  {
+    id: 23,
+    image1: img23,
+    image2: img23_1,
+    title: "BỘ KEYCAP CMK RESONANCE",
+    price: 650000,
+    salePercent: 8,
+    brand: "CMK",
+    type: "keycap bộ",
+  },
+  {
+    id: 24,
+    image1: img24,
+    image2: img24_1,
+    title: "BỘ KEYCAP CMK RESONANCE",
+    price: 520000,
+    salePercent: 4,
+    brand: "STOOGES",
+    type: "keycap bộ",
+  },
+  {
+    id: 25,
+    image1: img25,
+    image2: img25_1,
+    title: "BỘ KEYCAP QUANTUM COMPUTER",
+    price: 800000,
+    salePercent: 13,
+    brand: "MG",
+    type: "keycap bộ",
+  },
+  {
+    id: 26,
+    image1: img26,
+    image2: img26_1,
+    title: "BỘ KEYCAP THẢO THẦN NAHIDA",
+    price: 600000,
+    salePercent: 8,
+    brand: "CMK",
+    type: "keycap bộ",
+  },
+  {
+    id: 27,
+    image1: img27,
+    image2: img27_1,
+    title: "BỘ KEYCAP SA EGYPT [PRE-ORDER]",
+    price: 1290000,
+    salePercent: 7,
+    brand: "OSIRIS",
+    type: "keycap bộ",
+  },
+  {
+    id: 28,
+    image1: img28,
+    image2: img28_1,
+    title: "BỘ KEYCAP MG RAINBOW",
+    price: 850000,
+    salePercent: 24,
+    brand: "MG",
+    type: "keycap bộ",
+  },
+  {
+    id: 29,
+    image1: img29,
+    image2: img29_1,
+    title: "BỘ KEYCAP VIRTUAL WAR ORIGIN",
+    price: 550000,
     salePercent: 18,
-    brand: "Xt-design",
-    type: "Keycap bộ",
+    brand: "CMK",
+    type: "keycap bộ",
+  },
+  {
+    id: 30,
+    image1: img30,
+    image2: img30_1,
+    title: "BỘ KEYCAP P5 PERSONA5",
+    price: 900000,
+    salePercent: 11,
+    brand: "XT-DESIGN",
+    type: "keycap bộ",
+  },
+  {
+    id: 31,
+    image1: img31,
+    image2: img31_1,
+    title: "[HÀNG SẮP VỀ] BÀN PHÍM CƠ KIT FINALKEY V81 PLUS",
+    price: 2700000,
+    salePercent: 7,
+    brand: "FINALKEY",
+    type: "kit",
+  },
+  {
+    id: 32,
+    image1: img32,
+    image2: img32_1,
+    title: "BỘ KEYCAP SA COLOURFUL DREAM",
+    price: 1100000,
+    salePercent: null,
+    brand: "OSIRIS",
+    type: "keycap bộ",
+  },
+  {
+    id: 33,
+    image1: img33,
+    image2: img33_1,
+    title: "PLATE NHÔM CHO BÀN PHÍM CƠ",
+    price: 270000,
+    salePercent: null,
+    brand: "KICAP",
+    type: "phụ kiện",
+  },
+  {
+    id: 34,
+    image1: img34,
+    image2: img34_1,
+    title: "BÀN PHÍM CƠ CUSTOM INFIVERSE INFI75",
+    price: 2500000,
+    salePercent: null,
+    brand: "INFIVERSE",
+    type: "bàn phím cơ",
+  },
+  {
+    id: 35,
+    image1: img35,
+    image2: img35_1,
+    title: "SWITCH WS MORANDI",
+    price: 10500,
+    salePercent: 10,
+    brand: "WUQUE STUDIO",
+    type: "switch",
+  },
+  {
+    id: 36,
+    image1: img36,
+    image2: img36_1,
+    title: "BỘ KEYCAP CHOCOLATE DONUT",
+    price: 699000,
+    salePercent: null,
+    brand: "CMK",
+    type: "keycap bộ",
+  },
+  {
+    id: 37,
+    image1: img37,
+    image2: img37_1,
+    title: "BÀN PHÍM CƠ CMK75 | FL-ESPORTS",
+    price: 2550000,
+    salePercent: 14,
+    brand: "FL - ESPORTS",
+    type: "bàn phím cơ",
+  },
+  {
+    id: 38,
+    image1: img38,
+    image2: img38_1,
+    title: "BỘ KEYCAP SILENT FOREST",
+    price: 780000,
+    salePercent: null,
+    brand: "CMK",
+    type: "keycap bộ",
+  },
+  {
+    id: 39,
+    image1: img39,
+    image2: img39_1,
+    title: "TÚI ĐỰNG BÀN PHÍM, CHỐNG SỐC, CHỐNG XƯỚC",
+    price: 80000,
+    salePercent: 14,
+    brand: "KELOWNA",
+    type: "phụ kiện bàn phím",
+  },
+  {
+    id: 40,
+    image1: img40,
+    image2: img40_1,
+    title: "BỘ KEYCAP CHERRY DORAEMON",
+    price: 525000,
+    salePercent: 5,
+    brand: "STOOGES",
+    type: "keycap bộ",
   },
 ];
 
