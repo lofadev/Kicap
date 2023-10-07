d6c606585a8a9b64abde27ff3f8a399f51b3123a
LofA <82990903+lofadev@users.noreply.github.com>
2023-10-07 10:49:32 +0700

    product category page (#9)

--- data.js
+++ data.js
@@ -2841,3 +2841,122 @@ export const policys = [
     ],
   },
 ];
+
+export const nav = [
+  {
+    id: 1,
+    name: 'Trang chủ',
+    hasChild: false,
+    navigate: '/',
+  },
+  {
+    id: 2,
+    name: 'Keycap bộ',
+    hasChild: true,
+    navigate: '/keycap-bo',
+    children: [
+      { id: 1, title: 'Keycap Cherry', navigate: '/keycap-cherry' },
+      { id: 2, title: 'Keycap xuyên led', navigate: '/keycap-xuyen-led' },
+      { id: 3, title: 'Keycap SA', navigate: '/keycap-sa' },
+      { id: 4, title: 'Keycap XDA', navigate: '/keycap-xda' },
+      { id: 5, title: 'Keycap OEM', navigate: '/keycap-oem' },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Mods phím',
+    hasChild: false,
+    navigate: '/mods-ban-phim-co',
+  },
+  {
+    id: 4,
+    name: 'Pre-order',
+    hasChild: false,
+    navigate: '/pre-order',
+  },
+  {
+    id: 5,
+    name: 'Decor',
+    hasChild: false,
+    navigate: '/den-decor',
+  },
+  {
+    id: 6,
+    name: 'Sản phẩm',
+    navigate: '/collections/all',
+    hasMega: true,
+    children: [
+      {
+        id: 1,
+        title: 'Keycap bộ',
+        navigate: '/keycap-bo',
+        sub_children: [
+          {
+            id: 1,
+            name: 'Silent forest',
+            navigate: '/bo-keycap-silent-forest',
+          },
+          {
+            id: 2,
+            name: 'SA Harry Potter',
+            navigate: '/bo-keycap-sa-harry-potter-snake',
+          },
+          {
+            id: 3,
+            name: 'Keycap Polar Day',
+            navigate: '/bo-keycap-polar-day-oem-xuyen-led',
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: 'Keycap lẻ',
+        navigate: '/keycap-le',
+        sub_children: ['PIKACHU ALU', 'CARD VGA', 'CỜ ĐẢNG BÚA LIỀM'],
+      },
+      {
+        id: 3,
+        title: 'Đèn decor',
+        navigate: '/keycap-le',
+        sub_children: [],
+      },
+      {
+        id: 4,
+        title: 'Bàn phím cơ',
+        navigate: '/ban-phim-co',
+
+        sub_children: ['Monsgeek M1W', 'FL CMK75', 'Finalkey V81 Plus'],
+      },
+      {
+        id: 5,
+        title: 'Switch',
+        navigate: '/switch',
+        sub_children: ['WS Morandi', 'KTT Matcha', 'Akko Cream Yellow Pro'],
+      },
+      {
+        id: 6,
+        title: 'Phụ kiện',
+        navigate: '/phu-kien',
+        sub_children: ['Túi đựng bàn phím', 'Dụng cụ thay keycap', 'Chổi quét phím'],
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'Blog',
+    hasChild: false,
+    navigate: '/tin-tuc',
+  },
+  {
+    id: 8,
+    name: 'Về kicap',
+    hasChild: true,
+    navigate: '/gioi-thieu',
+    children: [
+      { id: 1, title: 'Giới thiệu', navigate: '/gioi-thieu' },
+      { id: 2, title: 'Liên hệ', navigate: '/lien-he' },
+      { id: 3, title: 'Chính sách bảo hành', navigate: '/policy/chinh-sach-bao-hanh' },
+      { id: 4, title: 'Chính sách đổi trả', navigate: '/policy/chinh-sach-doi-tra-hang-hoan-tien' },
+    ],
+  },
+];
