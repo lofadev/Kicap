6d40bcc07f5225cb00833fc55ecca480eaf3dc92
LofA <82990903+lofadev@users.noreply.github.com>
2024-03-05 15:39:40 +0700

    config redux persist (#24)

--- src/redux/slides/UserSlide.js
+++ src/redux/slides/UserSlide.js
@@ -7,14 +7,50 @@ export const userSlide = createSlice({
     name: '',
     email: '',
     phone: '',
-    city: '',
+    address: '',
+    province: '',
+    avatar: '',
     accessToken: '',
     refreshToken: '',
-    avatar: '',
     isAdmin: '',
   },
   reducers: {
-    updateUser: (state, action) => {},
+    updateUser: (state, action) => {
+      const {
+        name = '',
+        email = '',
+        accessToken = '',
+        address = '',
+        phone = '',
+        avatar = '',
+        _id = '',
+        isAdmin,
+        province = '',
+        refreshToken = '',
+      } = action.payload;
+      state.id = _id ? _id : state.id;
+      state.name = name ? name : state.name;
+      state.email = email ? email : state.email;
+      state.phone = phone ? phone : state.phone;
+      state.address = address ? address : state.address;
+      state.province = province ? province : state.province;
+      state.avatar = avatar ? avatar : state.avatar;
+      state.accessToken = accessToken ? accessToken : state.accessToken;
+      state.refreshToken = refreshToken ? refreshToken : state.refreshToken;
+      state.isAdmin = isAdmin ? isAdmin : state.isAdmin;
+    },
+    resetUser: (state) => {
+      state.id = '';
+      state.name = '';
+      state.email = '';
+      state.phone = '';
+      state.address = '';
+      state.province = '';
+      state.avatar = '';
+      state.accessToken = '';
+      state.refreshToken = '';
+      state.isAdmin = false;
+    },
   },
 });
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import counterReducer from './slides/CounterSlide.js';
+import userReducer from './slides/UserSlide.js';
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  user: userReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
