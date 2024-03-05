d727a9d6eb86a6312d77f51dcd6197e2513a787e
Truong Phuoc Thien An <contact.lofa@gmail.com>
2024-03-05 15:37:44 +0700

    config redux persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App.jsx';
 import './main.scss';
-import store from './redux/store.js';
+import { persistor, store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
