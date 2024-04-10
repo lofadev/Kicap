8cf090a95b2e871e2b931afe311f1599db9f3f84
LofA <82990903+lofadev@users.noreply.github.com>
2024-04-10 09:25:04 +0700

    build frame of order page and add order page (#46)

--- src/pages/admin/Order/AddOrder/AddOrder.jsx
+++ src/pages/admin/Order/AddOrder/AddOrder.jsx
@@ -1,5 +1,54 @@
+import { useDispatch } from 'react-redux';
+import Box from '~/components/Admin/Box/Box';
+import HeadingBreadCrumb from '~/components/Admin/HeadingBreadCrumb/HeadingBreadCrumb';
+import './AddOrder.scss';
+import FormSearch from '~/components/Admin/FormSearch/FormSearch';
+import { useState } from 'react';
+import DataTable from '~/components/DataTable/DataTable';
+
 const AddOrder = () => {
-  return <div>AddOrder</div>;
+  const dispatch = useDispatch();
+  const [inputSearch, setInputSearch] = useState('');
+
+  const handleChangeInputSearch = (e) => {
+    setInputSearch(e.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log(inputSearch);
+  };
+
+  return (
+    <div className='add-order'>
+      <HeadingBreadCrumb>Lập đơn hàng</HeadingBreadCrumb>
+      <Box title='Thông tin đơn hàng'>
+        <div className='add-order-main'>
+          <div className='products'>
+            <h2>Lựa chọn mặt hàng cần bán</h2>
+            <FormSearch
+              placeholder={'Nhập tên sản phẩm cần tìm'}
+              value={inputSearch}
+              handleOnChange={handleChangeInputSearch}
+              handleSearch={handleSearch}
+            ></FormSearch>
+          </div>
+          <div className='cart'>
+            <div className='cart-list'>
+              <h2>Danh sách mặt hàng đã chọn</h2>
+              <DataTable
+                head={['STT', 'Tên sản phẩm', 'Số lượng', 'Giá', 'Thành tiền']}
+                rows={[]}
+                isOrder
+                keys={['index', 'productName', 'quantity', 'price', 'totalPrice']}
+              ></DataTable>
+            </div>
+
+            <div className='order-info'></div>
+          </div>
+        </div>
+      </Box>
+    </div>
+  );
 };
 
 export default AddOrder;
