c03b783a1a5049692d57e5932e513381cdaa276a
LofA <contact.lofa@gmail.com>
2024-05-21 08:13:04 +0700

    Implement feature reset password

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,15 +1,6 @@
 import queryString from 'query-string';
-import { useLocation } from 'react-router-dom';
 
 const useQuery = () => {
-  const location = useLocation();
-  // const query = location.search.slice(1, location.search.length);
-  // const queryArr = query.split('&');
-  // const result = queryArr.reduce((acc, query) => {
-  //   const [name, value] = query.split('=');
-  //   acc[name] = value;
-  //   return acc;
-  // }, {});
   const result = queryString.parse(location.search);
   return result;
 };
